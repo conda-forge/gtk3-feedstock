4b31772c6b22f843a8b8faf4167874ab5a065e96
Ryan Volz <ryan.volz@gmail.com>
2021-01-29 17:13:48 -0500

    Do full native build first if cross-compiling to generate gir.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,6 @@
 
 set -ex
 
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-  # interferes with build env python that we actually use for the build
-  unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
-fi
-
 # get meson to find pkg-config when cross compiling
 export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
 
@@ -33,6 +28,42 @@ meson_config_args=(
 # ensure that the post install script is ignored
 export DESTDIR="/"
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
+  (
+    mkdir -p native-build
+    pushd native-build
+
+    export CC=$CC_FOR_BUILD
+    export AR=($CC_FOR_BUILD -print-prog-name=ar)
+    export NM=($CC_FOR_BUILD -print-prog-name=nm)
+    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
+    export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
+
+    # Unset them as we're ok with builds that are either slow or non-portable
+    unset CFLAGS
+    unset CPPFLAGS
+    export host_alias=$build_alias
+    export PKG_CONFIG_PATH=$BUILD_PREFIX/lib/pkgconfig
+
+    meson "${meson_config_args[@]}" \
+        --buildtype=release \
+        --prefix=$BUILD_PREFIX \
+        -Dlibdir=lib \
+        --wrap-mode=nofallback \
+        ..
+
+    # This script would generate the functions.txt and dump.xml and save them
+    # This is loaded in the native build. We assume that the functions exported
+    # by glib are the same for the native and cross builds
+    export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-save.sh
+    ninja -v -j ${CPU_COUNT}
+    ninja install -j ${CPU_COUNT}
+    popd
+  )
+  export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-load.sh
+fi
+
 meson setup builddir \
     ${MESON_ARGS} \
     "${meson_config_args[@]}" \
@@ -42,14 +73,3 @@ meson setup builddir \
     --wrap-mode=nofallback
 ninja -v -C builddir -j ${CPU_COUNT}
 ninja -C builddir install -j ${CPU_COUNT}
-
-cd $PREFIX
-find . '(' -name '*.la' -o -name '*.a' ')' -delete
-rm -rf $(echo "
-share/applications
-share/gtk-doc
-share/man
-bin/gtk3-demo*
-bin/gtk3-icon-browser
-bin/gtk3-widget-factory
-")
