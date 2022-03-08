60b9e7523f1b46d747d79006ac69b4a7f88c3d74
Ryan Volz <ryan.volz@gmail.com>
2022-03-08 12:22:27 -0500

    Try to fix run_test.py with xvfb on Linux by initializing display first.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,18 +1,20 @@
-import gi
-gi.require_version("Gtk", "3.0")
-from gi.repository import GLib, Gtk
-assert(3 == Gtk.get_major_version())
-
 # start an Xvfb virtual display if we can (if xvfbwrapper is installed)
 try:
     from xvfbwrapper import Xvfb
-except ModuleNotFoundError:
+except (ModuleNotFoundError, ImportError):
+    print("Using physical display")
     vdisplay = None
 else:
+    print("Using Xvfb virtual display")
     vdisplay = Xvfb(width=1024, height=768, colordepth=24)
     vdisplay.start()
 
 try:
+    import gi
+    gi.require_version("Gtk", "3.0")
+    from gi.repository import GLib, Gtk
+    assert(3 == Gtk.get_major_version())
+
     window = Gtk.Window(title="Hello World")
     window.connect("destroy", Gtk.main_quit)
     window.show_all()
